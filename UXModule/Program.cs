94e0addfe6ccaeaaa39ab94a51217f221204ea02
gr1310 <garimaranjan.iitpkd@gmail.com>
2024-08-20 16:06:19 +0530

    UX module added

---
+++ UXModule/Program.cs
@@ -0,0 +1,27 @@
+﻿using SecurityProviderModule;
+
+namespace UXModule
+{
+    public class Program
+    {
+        public void Main()
+        {
+            ISecurityProvider provider;
+            String userInput = Console.ReadLine();
+
+            while (userInput != "quit")
+            {
+                if (userInput == "1")
+                {
+                    provider = new AntiVirusSecurityProvider();
+                    provider.Scan();
+                }
+                else if (userInput == "2")
+                {
+                    provider = new AccountSecurityProvider();
+                    provider.Scan();
+                }
+            }
+        }
+    }
+}
